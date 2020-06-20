3a5509b6b46a56bb3942478f0a1c115a260425f9
Kevin Wang <kwang7@horizon.csueastbay.edubrew>
2020-06-20 14:35:25 -0700

    added message function for ending of timer

--- script.js
+++ script.js
@@ -8,17 +8,26 @@
         //These scores should be stored in the scores category, in last ranging in order from highest to lowest.
         //The player should get a prompt asking for their name once they get their score.
         //The player should be able to play again with a reset button.        
-var timeEl = document.querySelector(".time")
+var timeEl = document.querySelector("time")
 var mainEl = document.getElementById ("main");
 var secondsLeft= 120
 
-function setTimer () {
+function setTime() {
     var timerInterval = setInterval(function() {
     secondsLeft--;
-    timeEl.textContent = secondsLeft + "time left until end of test.";
-    if(secondsLeft) === 0) {
+    timeEl.textContent = secondsLeft + "time left until end of quiz.";
+    if(secondsLeft === 0) {
     clearInterval(timerInterval);
     sendMessage();    
     }
     }, 1000);
 }
+
+function sendMessage() {
+    timeEl.textContent = " ";
+    var imgEl = document.createElement("text");    
+    imgEl.setAttrribute("time's up");
+    mainEl.appendChild(imgEl);
+}
+
+setTime ();
