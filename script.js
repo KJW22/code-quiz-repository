d6b9cc0d8d28269b28267ffd226e0170e1b01e0a
Kevin Wang <kwang7@horizon.csueastbay.edubrew>
2020-06-20 15:01:49 -0700

    added trivia questions

--- script.js
+++ script.js
@@ -8,21 +8,24 @@
         //These scores should be stored in the scores category, in last ranging in order from highest to lowest.
         //The player should get a prompt asking for their name once they get their score.
         //The player should be able to play again with a reset button.        
-var timeEl = document.querySelector("time")
-var mainEl = document.getElementById ("main");
-var secondsLeft= 120
-
-function setTime() {
-    var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timeEl.textContent = secondsLeft + "time left until end of quiz.";
-    if(secondsLeft === 0) {
-    clearInterval(timerInterval);
-    sendMessage();    
-    }
-    }, 1000);
-}
-
+function startTimer(){
+            var counter = 5;
+            setInterval(function() {
+              counter--;
+              if (counter >= 0) {
+                span = document.getElementById("count");
+                span.innerHTML = counter;
+              }
+              if (counter === 0) {
+                  alert('sorry, out of time');
+                  clearInterval(counter);
+              }
+            }, 1000);
+          }
+          
+          $("#startClock").click(function(){
+              startTimer();
+          });
 function sendMessage() {
     timeEl.textContent = " ";
     var imgEl = document.createElement("text");    
@@ -31,3 +34,14 @@ function sendMessage() {
 }
 
 setTime ();
+//Each question is its own variable, with each one having an almost identical format.
+var question1 = {q: "What is Rex's last name?", a:"a: Cortez, b: Yepiz, c: Banderas, d: Salazar"};
+var question2 = {q: "Why is Agent Six known as Six?"};
+var question3 = {q: "What are Punk Busters?"}; 
+var question4 = {q: "Why did the Nanite Project Researchers blow up the Nanite Reactor?"};
+var question5 = {q: "Which U.S. President shares the same last name as Rex's best friend Noah?"};
+var question6 = {q: "Why does Van Kleiss always need to absorb new nanites?"};
+var question7 = {q: "Which Channel did Generator Rex air on?"};
+var question8 = {q: "What is the name of the band that sings the Generator Rex theme song?"};
+var question9 = {q: "Who is Hunter Cain?"};
+var question10 = {q: "What does EVO stand for?"};   
